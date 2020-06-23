6fc6e6022b278b11acce96f27e55280ff625b79f
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-06-23 12:22:35 -0500

    --- Auto-Git Commit ---

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+// init app and middlewares
 const app = express();
-const PORT = process.env.PORT || 1776;
-
+app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json());
 
+// set port and listen
+const PORT = process.env.PORT || 1776;
 app.listen(PORT, (req, res) => {
     console.info(`Server running on port ${PORT}`)
 })
